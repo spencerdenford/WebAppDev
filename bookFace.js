5c36a9b0e474d7944654b6020d8a6cc3da613d51
Spencer Denford <spencer.denford@ontariotechu.net>
2020-03-17 13:38:31 -0400

    added more to default page

--- bookFace.js
+++ bookFace.js
@@ -2,4 +2,14 @@ window.onload = function(){
     document.getElementById("home").onclick = function (){
         console.log("Hello World");
     }
+
+    var postText = document.getElementById("postText");
+    postText.onclick = function(){
+        postText.value = "";
+    }
+
+    document.getElementById("button2").onclick = function(){
+        console.log(postText.value);
+        postText.value = "";
+    }
 }
