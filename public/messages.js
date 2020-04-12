e45c79cc9b8d1ebea71cb96fa5b006f33f015114
Spencer Denford <46714697+spencerdenford@users.noreply.github.com>
2020-04-12 17:19:50 -0400

    Messages more dynamically

--- public/messages.js
+++ public/messages.js
@@ -1,11 +1,15 @@
 
 function messagesExpand(clicked) {
     clicked.children[0].children[2].innerHTML +=    `
+                                                    <div id="oldestsenttime">5:31pm</div>
                                                     <div id="messagecontent"> message history1 </div>
+                                                    <div id="senttime">5:34pm</div>
                                                     <div id="messagecontent"> message history2 </div>
+                                                    <div id="senttime">5:39pm</div>
                                                     <div id="messagecontent"> message history3 </div>
+                                                    <div id="senttime">5:41pm</div>
                                                     <div id="messagecontent"> message history4 </div>
-                                                    `; 
+                                                    `; // TODO: replace this with actual message history
 
     clicked.innerHTML +=    `
                             <input type="text" name="textfield" id="sendMessage" placeholder="Type Here:" />
@@ -15,13 +19,39 @@ function messagesExpand(clicked) {
         this.addEventListener("keyup", function(event) {
             // Number 13 is the "Enter" key on the keyboard
             if ((event.keyCode === 13) && (this.value != "")) {
+                var d = new Date();
+                var date = "";
+                var minutes = "";
+
+                if (d.getMinutes() < 10){
+                    minutes = `0${d.getMinutes()}`;
+                }
+                else {
+                    minutes = d.getMinutes();
+                }
+
+                if (d.getHours() == 0){
+                    date = `12:${minutes}am`;
+                }
+                else if (d.getHours() > 12){
+                    date = `${d.getHours()}:${minutes}pm`;
+                }
+                else {
+                    date = `${d.getHours()}:${minutes}am`;
+                }
+
+                // TODO: remove oldest message from db
                 // this moves all conversation history up one
-                for (i = 0; i < 3; i++){
-                    clicked.children[0].children[2].children[i].innerHTML = clicked.children[0].children[2].children[i + 1].innerHTML;
+                for (i = 1; i < 7; i += 2){
+                    clicked.children[0].children[2].children[i].innerHTML = clicked.children[0].children[2].children[i + 2].innerHTML;
+                    clicked.children[0].children[2].children[i - 1].innerHTML = clicked.children[0].children[2].children[i + 1].innerHTML;
                 }
                 // this is so the last non anchored message is replaces
-                clicked.children[0].children[2].children[3].innerHTML = clicked.children[0].children[4].innerHTML;
+                clicked.children[0].children[2].children[7].innerHTML = clicked.children[0].children[4].innerHTML;
+                clicked.children[0].children[2].children[6].innerHTML = clicked.children[0].children[3].innerHTML;
                 // this replaces the last anchored message
+                // TODO save time and message to db
+                clicked.children[0].children[3].innerHTML = `${date}`;
                 clicked.children[0].children[4].innerHTML = `${this.value}`;
 
                 this.value = "";
