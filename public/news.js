401bfe2c1d92abac0185c839f32140a8369ba716
Tyler Broda <tylerbroda11@gmail.com>
2020-04-11 22:04:58 -0400

    Styling changes

--- public/news.js
+++ public/news.js
@@ -17,14 +17,18 @@ $(document).ready(function() {
             for (var i = 0; i < data.articles.length; i++) {
                 newsDisplay += "<tr id='article'>";
                 newsDisplay += "<td id='articleDetails'>";
-                newsDisplay += "<div>"+"Source: "+articles[i].source.name+"</div>";
-                newsDisplay += "<div>"+"Author: "+articles[i].author+"</div>";
-                newsDisplay += "<div>"+"Title: "+articles[i].title+"</div>";
-                newsDisplay += "<div>"+"Description: "+articles[i].description+"</div>";
-                newsDisplay += "<div>"+"URL: "+articles[i].url+"</div>";
-                newsDisplay += "<img id='articlePic' src="+articles[i].urlToImage+">";
-                newsDisplay += "<div>"+"Published: "+articles[i].publishedAt+"</div>";
-                newsDisplay += "<div>"+"Content: "+articles[i].content+"</div>";
+                newsDisplay += "<a href='"+articles[i].url+"' target='_blank'>";
+                newsDisplay += "<div id='title'>"+articles[i].title+"</div>";
+                newsDisplay += "</a>";
+                newsDisplay += "<div id='source'>"+articles[i].source.name+"</div>";
+                newsDisplay += "<div id='datePublished'>"+"Published: "+articles[i].publishedAt+"</div>";
+                newsDisplay += "<div id='author'>"+"Author: "+articles[i].author+"</div>";
+                newsDisplay += "<div id='description'>"+"Description: "+articles[i].description+"</div>";
+                newsDisplay += "<div id='content'>"+"Content: "+articles[i].content+"</div>";
+                newsDisplay += "</td>";
+                newsDisplay += "<td id='articlePic' align='center'>";
+                newsDisplay += "<img id='image' src="+articles[i].urlToImage+">";
+                newsDisplay += "</td>";
                 newsDisplay += "</tr>";
             }
 
