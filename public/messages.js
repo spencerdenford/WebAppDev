c9d068e09013d1987d957339f77cdac7da89671a
Spencer Denford <spencer.denford@ontariotechu.net>
2020-04-15 16:20:38 -0400

    Fixed messaging

--- public/messages.js
+++ public/messages.js
@@ -6,44 +6,22 @@ const socket = io();
 var roomName = "";
 
 function messagesExpand(clicked) {
-    /*
-    clicked.children[0].children[2].innerHTML +=    `
-                                                    <div id="oldestsenttime">5:31pm</div>
-                                                    <div id="messagecontent"> message history1 </div>
-                                                    <div id="senttime">5:34pm</div>
-                                                    <div id="messagecontent"> message history2 </div>
-                                                    <div id="senttime">5:39pm</div>
-                                                    <div id="messagecontent"> message history3 </div>
-                                                    <div id="senttime">5:41pm</div>
-                                                    <div id="messagecontent"> message history4 </div>
-                                                    `; 
-    */
-    
+    // remove the display message
     clicked.children[0].children[2].innerHTML = "";
-    var timestr;
 
     for (i = 0; i < messagehistory.length; i++){
-        if (i == 0){
-            timestr = "oldestsenttime";
-        }
-        else {
-            timestr = "senttime";
-        }
-        clicked.children[0].children[2].innerHTML +=    `
-                                                        <div id="${timestr}">${messagetimes[i]}</div>
-                                                        <div id="messagecontent">${messagehistory[i]}</div>
-                                                        `;
+        clicked.children[0].children[2].innerHTML += `
+            <div id="senttime">${messagetimes[i]}</div>
+            <div id="messagecontent">${messagehistory[i]}</div>`;
     }
 
-    clicked.innerHTML +=    `
-                            <input type="text" name="textfield" id="sendMessage" placeholder="Type Here:" />
-                            `; // this is the text field
+    // add the text field
+    clicked.innerHTML += `<input type="text" name="textfield" id="sendMessage" placeholder="Type Here:" />`; 
 
     document.getElementById("sendMessage").onclick = function () {
-
         this.addEventListener("keyup", function(event) {
-            //console.log("testing");
-            // Number 13 is the "Enter" key on the keyboard
+            
+            // wait for the enter (13) key to be pressed!
             if ((event.keyCode === 13) && (this.value != "")) {
                 // socket.io
                 socket.emit('send message', {
@@ -52,10 +30,8 @@ function messagesExpand(clicked) {
                     username: null,
                 });
 
-                //console.log("testing2");
-
                 socket.on('broadcast message', function(data) {
-                    //console.log("CLIENT - Broadcast working");
+                    // this prevents the application from reading in messages from the sockets more than once
                     console.log(data.message);
                     if (data == lastmessage){
                         return;
@@ -63,78 +39,26 @@ function messagesExpand(clicked) {
 
                     lastmessage = data;
 
-                    //console.log('CLIENT - User said: ' + data.message);
-                    
-                    // get current date and make it correct format
-                    var d = new Date();
-                    var date = "";
-                    var minutes = "";
-
-                    if (d.getMinutes() < 10){
-                        minutes = `0${d.getMinutes()}`;
-                    }
-                    else {
-                        minutes = d.getMinutes();
-                    }
-
-                    if (d.getHours() == 0){
-                        date = `12:${minutes}am`;
-                    }
-                    else if (d.getHours() > 12){
-                        date = `${d.getHours() - 12}:${minutes}pm`;
-                    }
-                    else {
-                        date = `${d.getHours()}:${minutes}am`;
-                    }
-
                     // add new message and date to arrays
                     messagehistory.push(data.message);
-                    messagetimes.push(date);
-
-                    /*
-                    // this moves all conversation history up one
-                    for (i = 1; i < 7; i += 2){
-                        clicked.children[0].children[2].children[i].innerHTML = clicked.children[0].children[2].children[i + 2].innerHTML;
-                        clicked.children[0].children[2].children[i - 1].innerHTML = clicked.children[0].children[2].children[i + 1].innerHTML;
-                    }
-                    // this is so the last non anchored message is replaces
-                    clicked.children[0].children[2].children[7].innerHTML = clicked.children[0].children[4].innerHTML;
-                    clicked.children[0].children[2].children[6].innerHTML = clicked.children[0].children[3].innerHTML;
-                    // this replaces the last anchored message
-                    // TODO save time and message to db
-                    clicked.children[0].children[3].innerHTML = `${date}`;
-                    clicked.children[0].children[4].innerHTML = `${this.value}`;
-                    */
+                    messagetimes.push(formatDate());
                     
+                    // shorten my statements
+                    var messageBox = clicked.children[0];
+
                     // removes the prompt message and adds the sent message to the screen
-                    if (messagehistory.length == 1){
-                        clicked.children[0].children[3].remove();
-                        clicked.children[0].children[2].innerHTML +=    `
-                                                                        <div id="oldestsenttime">${messagetimes[0]}</div>
-                                                                        <div id="messagecontent">${messagehistory[0]}</div>
-                                                                        `;
-                    }
-                    else {
-                        clicked.children[0].children[2].innerHTML +=    `
-                                                                        <div id="senttime">${messagetimes[messagehistory.length - 1]}</div>
-                                                                        <div id="messagecontent">${messagehistory[messagehistory.length - 1]}</div>
-                                                                        `;
-                    }
+                    messageBox.children[3].innerHTML = "";
+                    messageBox.children[2].innerHTML += `
+                        <div id="senttime">${messagetimes[messagehistory.length - 1]}</div>
+                        <div id="messagecontent">${messagehistory[messagehistory.length - 1]}</div>`;
 
                     // add the above bloacked out code IF message history is too long
-                    var oldestsenttime;
                     if (messagehistory.length == 10){
-                        clicked.children[0].children[2].children[0].remove();
-                        clicked.children[0].children[2].children[0].remove();
+                        // remove the oldest message (message and time)
+                        messageBox.children[2].children[0].remove();
+                        messageBox.children[2].children[0].remove();
 
-                        oldestsenttime = clicked.children[0].children[2].children[0].innerHTML;
-                        
-                        clicked.children[0].children[2].innerHTML = 
-                            `<div id="oldestsenttime">${oldestsenttime}</div>`
-                            + clicked.children[0].children[2].innerHTML;
-
-                        clicked.children[0].children[2].children[1].remove();
-
+                        // this removes the oldest message from the messagehistory and messagetimes arrays
                         messagehistory.shift();
                         messagetimes.shift();
                     }
@@ -149,19 +73,49 @@ function messagesExpand(clicked) {
 
     // close the message history
     clicked.children[0].onclick = function () { 
-        // create display (most recent) message
+        // create the display (most recent) message
         if (messagehistory.length != 0){
-            this.children[2].innerHTML =    `
-                                            <div id="oldestsenttime">${messagetimes[messagehistory.length - 1]}</div>
-                                            <div id="messagecontent">${messagehistory[messagehistory.length - 1]}</div>
-                                            `;
+            this.children[2].innerHTML = `
+                <div id="senttime">${messagetimes[messagehistory.length - 1]}</div>
+                <div id="messagecontent">${messagehistory[messagehistory.length - 1]}</div>`;
         }
-        // close chat box
-        this.parentElement.children[1].remove();
 
-        // call function to refresh the  
+        // close chat box
+        if (this.parentElement.children[1] != null){
+            this.parentElement.children[1].remove();
+        }
+
+        // call function so we don't mess it up by clicking the text box
         this.onclick = function () { messagesExpand(this.parentElement) };
     }
+}
+
+function formatDate(){
+    // get current date and make it correct format
+    var d = new Date();
+    var date = "";
+    var minutes = "";
+
+    // This ensures that if the time is x:00 we display x:00 instead of just x:0
+    if (d.getMinutes() < 10){
+        minutes = `0${d.getMinutes()}`;
+    }
+    else {
+        minutes = d.getMinutes();
+    }
+
+    // formatting
+    if (d.getHours() == 0){ // 12am is 0
+        date = `12:${minutes}am`;
+    }
+    else if (d.getHours() > 12){ // this is after 12pm. we dont want a 24 hour clock
+        date = `${d.getHours() - 12}:${minutes}pm`;
+    }
+    else { // morning time :)
+        date = `${d.getHours()}:${minutes}am`;
+    }
+
+    return(date);
 }
 
 window.onload = function() {
@@ -176,6 +130,16 @@ window.onload = function() {
     var oldRoom = null;
 
     joinbutton.onclick = function(){
+        // this chunk of code here restores the message box to its original state 
+        var chatBox = document.getElementById("message").children[0];
+        if (chatBox.parentElement.children[1] != null){
+            chatBox.parentElement.children[1].remove();
+        }
+        chatBox.children[2].innerHTML = "";
+        chatBox.children[3].innerHTML = "Click here to send a message";
+        messagehistory = [];
+        messagetimes = [];
+
         //Displaying the chatbox for the room
         var chatBox = document.getElementById("message");
         chatBox.style.display = "block";
@@ -203,4 +167,6 @@ window.onload = function() {
 
         oldRoom = roomName;
     };
+
+
 }
